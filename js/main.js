f664e6c671cc290bda186bd9fe0c4463e49681bc
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-06 16:32:41 +0200

    Milestone, printed every mail in a list

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      email: "",
+      emailList: [],
     };
   },
   methods: {},
@@ -11,8 +11,11 @@ createApp({
     for (let i = 0; i < 10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function (email) {
-          console.log(email.data.response);
+        .then((email) => {
+          console.log(email.data);
+
+          const result = email.data.response;
+          this.emailList.push(result);
         });
     }
   },
